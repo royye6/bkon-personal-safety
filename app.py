d8f16e4b3cc35f7cefa5759c10428808d869ab24
royye6 <royye6@gmail.com>
2024-06-06 00:21:26 +0200

    database front-end connection

--- app.py
+++ app.py
@@ -2,53 +2,81 @@ from flask import Flask, render_template, request
 from flask import url_for, redirect
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime 
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from datetime import datetime
 
 app  = Flask(__name__)
 
+class Base(DeclarativeBase):
+    pass
+
 # database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy(model_class=Base)
+
+db.init_app(app)
 
 # database models
 class ContactForm(db.Model):
     __tablename__ = 'contact_form'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    message = db.Column(db.String, nullable=False)
+
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(db.String, nullable=False)
+    email: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+
 
 class Ebook(db.Model):
-    __tablename__ = 'EBOOKS'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
-    image_path = db.Column(db.String, nullable=False)
+    __tablename__ = 'ebooks'
+
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(db.String, nullable=False)
+    price: Mapped[str] = mapped_column(db.String, nullable=False)
+    image_path: Mapped[str] = mapped_column(db.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Ebook('{self.id}', '{self.title}', '{self.price}', '{self.image_path}>)"
+    
 
 class PepperSpray(db.Model):
-    __tablename__ = 'PEPPERSPRAY'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
-    image_path = db.Column(db.String, nullable=False)
+    __tablename__ = 'pepperspray'
+
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(db.String, nullable=False)
+    price: Mapped[str] = mapped_column(db.String, nullable=False)
+    image_path: Mapped[str] = mapped_column(db.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<PepperSpray('{self.id}', '{self.title}', '{self.price}', '{self.image_path}>)"
+    
 
 class StunDevice(db.Model):
-    __tablename__ = 'STUNDEVICES'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
-    image_path = db.Column(db.String, nullable=False)
+    __tablename__ = 'stundevices'
+
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(db.String, nullable=False)
+    price: Mapped[str] = mapped_column(db.String, nullable=False)
+    image_path: Mapped[str] = mapped_column(db.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<StunDevice('{self.id}', '{self.title}', '{self.price}', '{self.image_path}>)"
+    
 
 class Wearable(db.Model):
-    __tablename__ = 'WEARABLES'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
-    image_path = db.Column(db.String, nullable=False)
+    __tablename__ = 'wearables'
+
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(db.String, nullable=False)
+    price: Mapped[str] = mapped_column(db.String, nullable=False)
+    image_path: Mapped[str] = mapped_column(db.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Wearable('{self.id}', '{self.title}', '{self.price}', '{self.image_path}>)"
 
 
 # index page route
-@app.route("/", methods=["POST", "GET"] )
+@app.route("/", methods=["POST"] )
 def index():
     return render_template("index.html")
 
@@ -60,9 +88,14 @@ def m_app():
 
 
 # store page route
-@app.route("/shop", methods=["GET"])
+@app.route("/shop")
 def shop():
-    return render_template("shop.html")
+    ebooks = db.session.execute(db.select(Ebook).order_by(Ebook.id)).scalars()
+    pepperspray = db.session.execute(db.select(PepperSpray).order_by(PepperSpray.id)).scalars()
+    stundevices = db.session.execute(db.select(StunDevice).order_by(StunDevice.id)).scalars()
+    wearables = db.session.execute(db.select(Wearable).order_by(Wearable.id)).scalars()
+    return render_template("shop.html", ebooks=ebooks, pepperspray=pepperspray, 
+                           stundevices=stundevices, wearables=wearables)
 
 
 
